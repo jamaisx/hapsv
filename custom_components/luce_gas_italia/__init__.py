58e0f36b7a181f568baa21bfab7ecdc6c64c8adc
jamaisx <jamaisx@gmail.com>
2026-04-03 15:42:53 +0200

    Update __init__.py

--- custom_components/luce_gas_italia/__init__.py
+++ custom_components/luce_gas_italia/__init__.py
@@ -2,11 +2,13 @@ from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
 from .const import DOMAIN
 
+# Definiamo le piattaforme da caricare (nel tuo caso solo sensor)
+PLATFORMS = ["sensor"]
+
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up da Config Flow."""
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
-    )
+    # Nota la 's' finale e la lista [PLATFORMS]
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
